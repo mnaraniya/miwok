ccae24ea498dceccbf58680494822b8898d54c93
Mukesh <mnaraniya@gmail.com>
2017-08-13 12:20:45 +0530

    splash screen but with error of just showing once

---
+++ app/src/main/java/com/example/android/miwok/SplashActivity.java
@@ -0,0 +1,12 @@
+package com.example.android.miwok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
